ea185181f4b199da72e451d241908d28237c3ec3
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-01-04 14:01:20 +0200

    Fetch error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const getBody = async (url) => {
     const res = await fetch(url);
+    if(!res.ok) {
+        throw new Error(`Error could not get data from ${url}, we get status: ${res.status} `)
+    }
     const body = await res.json()
     return body;
 }
 
-getBody('https://swapi.dev/api/people/11/')
+getBody('https://swapi.dev/api/people/21111/')
     .then((body)=>{
         console.log(body)
+    })
+    .catch((err) => {
+        console.error('Could not fetch: ', err)
     })
