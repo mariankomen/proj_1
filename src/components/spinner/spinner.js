0eed8b0c954bad1b3afa9efae2dab03169dd04ef
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-01-05 16:14:25 +0200

    Spinner and <React.Fragment>

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css'
+
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-dual-ring-q1uie8m9dh">
+            <div className="ldio-hjfv6ix34j5">
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Spinner;
